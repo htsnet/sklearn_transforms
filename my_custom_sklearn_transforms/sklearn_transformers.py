e5fca36eea903ab716b1f26939d0e92acd5482f3
Hamilton Tenório da Silva <hamilton@hts-net.com.br>
2020-08-21 09:01:03 -0300

    Update sklearn_transformers.py
    Inserting SMOTE in pipeline

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,6 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 import pandas as pd
+from imblearn.over_sampling import SMOTE, ADASYN
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -8,13 +9,12 @@ class DropColumns(BaseEstimator, TransformerMixin):
 
     def fit(self, X, y=None):
         return self
-
+    
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
-
     
 #Preenche o campo indicado (coluna) com o valor desejado    
 class FillWith(BaseEstimator, TransformerMixin):
@@ -33,18 +33,7 @@ class FillWith(BaseEstimator, TransformerMixin):
         # Retornamos um novo dataframe
         return data     
     
-#cria uma coluna com TRUE para os faltosos e FALSE para os demais    
-class Faltosos(BaseEstimator, TransformerMixin):
-    def fit(self, X, y=None):
-        return self
-    
-    def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
-        data = X.copy()
-        #faz a criação da coluna "FALTOSO" 
-        data['FALTOSO'] = data.apply(lambda x: True if (x['H_AULA_PRES'] <= x['FALTAS']) else False, axis=1)
-        # Retornamos um novo dataframe
-        return data     
+
     
 #cria uma coluna com média geral das notas
 class MediaGeral(BaseEstimator, TransformerMixin):
@@ -52,7 +41,8 @@ class MediaGeral(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X):
-        def AvaliaCaso(faltoso, nota_de, nota_em, nota_mf, nota_go):
+        #def AvaliaCaso(faltoso, nota_de, nota_em, nota_mf, nota_go):
+        def AvaliaCaso(nota_de, nota_em, nota_mf, nota_go):            
             conta = 0
             valores = 0
             if nota_de > 0 and not pd.isna(nota_de):
@@ -76,12 +66,13 @@ class MediaGeral(BaseEstimator, TransformerMixin):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
         #faz a criação da coluna "MEDIA_GERAL" sem considerar 0 para não faltosos
-        data['MEDIA_GERAL'] = data.apply(lambda x: AvaliaCaso(x.FALTOSO, x.NOTA_DE, x.NOTA_EM, x.NOTA_MF, x.NOTA_GO), axis=1)
+        #data['MEDIA_GERAL'] = data.apply(lambda x: AvaliaCaso(x.FALTOSO, x.NOTA_DE, x.NOTA_EM, x.NOTA_MF, x.NOTA_GO), axis=1)
+        data['MEDIA_GERAL'] = data.apply(lambda x: AvaliaCaso(x.NOTA_DE, x.NOTA_EM, x.NOTA_MF, x.NOTA_GO), axis=1)
         # Retornamos um novo dataframe
         return data     
     
     
-#Altera nota quando está NaN ou 0 e limita em 10
+#Altera nota quando está NaN ou 0 e limita a nota em 10
 class NotaUnica(BaseEstimator, TransformerMixin):
     def __init__(self, qual):
         self.qual = qual
@@ -90,15 +81,14 @@ class NotaUnica(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X):
-        def AvaliaNota(faltoso, nota_media, qual, nota):
+        #def AvaliaNota(faltoso, nota_media, qual, nota):
+        def AvaliaNota(nota_media, qual, nota):    
             #verifica se nota é 0 ou NaN
             if pd.isna(nota) or nota == 0:
-                #print(faltoso, nota_media, qual, nota)
-#                 if faltoso:
-#                     nota = 0
-#                 else:
-                nota = nota_media
-                #print("mudou", nota)
+                if nota_media > 0:
+                    nota = 10
+                else:
+                    nota = 0
             elif nota > 10:    
               nota = 10
             return nota
@@ -106,7 +96,8 @@ class NotaUnica(BaseEstimator, TransformerMixin):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
         #faz verificação se a nota é 0 ou NaN e coloca novo valor
-        data[self.qual] = data.apply(lambda x: AvaliaNota(x.FALTOSO, x.MEDIA_GERAL, self.qual, x[self.qual]), axis=1)
+        #data[self.qual] = data.apply(lambda x: AvaliaNota(x.FALTOSO, x.MEDIA_GERAL, self.qual, x[self.qual]), axis=1)
+        data[self.qual] = data.apply(lambda x: AvaliaNota(x.MEDIA_GERAL, self.qual, x[self.qual]), axis=1)
         # Retornamos um novo dataframe
         return data       
     
@@ -122,4 +113,51 @@ class Dificuldade(BaseEstimator, TransformerMixin):
         #faz a criação da coluna "DIFICULDADE" 
         data['DIFICULDADE'] = data.apply(lambda x: True if (x['MEDIA_GERAL'] == 0) else False, axis=1)
         # Retornamos um novo dataframe
-        return data      
+        return data     
+    
+    
+#transforma um campo em determinado tipo
+class TrocaTipo(BaseEstimator, TransformerMixin):
+    def __init__(self, coluna, tipo):
+        self.coluna = coluna
+        self.tipo = tipo
+    
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        # faz a transformação do tipo da coluna
+        data[self.coluna] = data[self.coluna].astype(self.tipo) 
+        # Retornamos um novo dataframe
+        return data 
+    
+    
+#executa o SMOTE
+class ExecutaSmote(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Definição das colunas que serão features (nota-se que a coluna NOME não está presente)
+        features = [
+            'REPROVACOES_DE', 'REPROVACOES_EM', "REPROVACOES_MF", "REPROVACOES_GO",
+            "NOTA_DE", "NOTA_EM", "NOTA_MF", "NOTA_GO",
+            "INGLES", "TAREFAS_ONLINE", 'H_AULA_PRES', 'FALTAS'
+        ]
+        # Definição da variável-alvo
+        target = ["PERFIL"]
+        data = X.copy()
+        
+        # Preparação dos argumentos para os métodos da biblioteca ``scikit-learn``
+        X_inicial = X[features]
+        y_inicial = X[target]
+        
+        X_apoio, y = SMOTE().fit_sample(X_inicial, y_inicial.values.ravel())
+        y_apoio = pd.DataFrame(y, columns=target)
+        X_saida = pd.DataFrame(X_apoio, columns=X_inicial.columns)
+        data = pd.merge(X_saida, y_apoio, left_index=True, right_index=True)
+        
+        # Retornamos um novo dataframe
+        return data
